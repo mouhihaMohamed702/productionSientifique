ef90fb307dcfd1a9d6e136b61f597077ff10811c
mouhihaMohamed702 <mouhihamohamed@gmail.com>
2021-06-07 21:16:51 +0000

    the last commit of back-end

---
+++ springboot/project/src/main/java/com/mohamed/project/load/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.mohamed.project.load.response;
+
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
